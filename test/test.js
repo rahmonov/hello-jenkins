b2ae647ad734335d6d22b0e504e027208cb10a12
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-03-26 23:33:49 +0500

    Fix test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function(){
         it('responds with hello world', function(done){
-                request(app).get('/').expect('hello world!', done);
+                request(app).get('/').expect('helloooo change by Jenkins CI & CD!!!', done);
         });
 });
